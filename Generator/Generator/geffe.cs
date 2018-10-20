82517435eda71fde53c308ef4398ee44eed3a732
Piotr Kontowicz <przemyslowiec@gmail.com>
2018-10-20 17:49:00 +0200

    UI

---
+++ Generator/Generator/geffe.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generator
+{
+    class geffe
+    {
+        private lfsr lfsr1;
+        private lfsr lfsr2;
+        private lfsr lfsr3;
+
+        geffe(lfsr _1, lfsr _2, lfsr _3)
+        {
+            lfsr1 = _1;
+            lfsr2 = _2;
+            lfsr3 = _3;
+        }
+
+        public int next()
+        {
+            int toReturn = 0;
+            switch(lfsr1.shift())
+            {
+                case 0:
+                    {
+                        toReturn = lfsr2.shift();
+                        lfsr3.shift();
+                    }break;
+                case 1:
+                    {
+                        toReturn = lfsr3.shift();
+                        lfsr2.shift();
+                    }break;
+            }
+            return toReturn;
+        }
+    }
+}
---
+++ Generator/Generator/lfsr.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generator
+{
+    class lfsr
+    {
+        private int[] register;
+        private int[] positions;
+        
+        private void moveRight(int t)
+        {
+            for(int i = register.Length - 1; i > 0; i--)
+            {
+                register[i] = register[i-1];
+            }
+            register[0] = t;
+        }
+        public int shift()
+        {
+            List<int> tmp = new List<int>();
+            int output = register[register.Length - 1];
+            for (int i = 0; i < positions.Length; ++i)
+            {
+                tmp.Add(register[positions[i]]);
+            }
+
+            int[] valuesCalc = tmp.ToArray();
+            int result = 0;
+            foreach (int num in valuesCalc)
+                result += num;
+            result = result % 2;
+
+            moveRight(result);
+
+            return output;
+        }
+        
+        public lfsr(string polynomial, string begin)
+        {
+            register = new int[begin.Length];
+            string[] test = polynomial.Split('+');
+            List<int> tmp = new List<int>();
+            for (int i = 0; i < test.Length; ++i)
+            {
+                if(test[i][0]=='x')
+                {
+                    if(test[i] == "x")
+                        tmp.Add(0);
+                    else
+                        tmp.Add(int.Parse(test[i].Remove(0, 1))-1);
+                }
+            }
+
+            for(int i = 0; i < begin.Length; i++)
+            {
+                register[i] = int.Parse(begin[i].ToString());
+            }
+
+            positions = tmp.ToArray();
+        }
+    }
+}
