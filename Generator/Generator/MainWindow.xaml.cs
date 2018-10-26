1712453582276651f0fb02471d26033bcbb32a5a
Piotr Kontowicz <przemyslowiec@gmail.com>
2018-10-26 23:37:41 +0200

    Generate in new task

--- Generator/Generator/MainWindow.xaml.cs
+++ Generator/Generator/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -25,184 +26,12 @@ namespace Generator
 
     public partial class MainWindow : Window
     {
-        public static double erf(double x)
-        {
-            double y, z;
-            double[] T =
-                {
-                    9.60497373987051638749E0,
-                    9.00260197203842689217E1,
-                    2.23200534594684319226E3,
-                    7.00332514112805075473E3,
-                    5.55923013010394962768E4
-                };
-            double[] U =
-                {
-                    //1.00000000000000000000E0,
-                    3.35617141647503099647E1,
-                    5.21357949780152679795E2,
-                    4.59432382970980127987E3,
-                    2.26290000613890934246E4,
-                    4.92673942608635921086E4
-                };
-
-            if (Math.Abs(x) > 1.0)
-                return (1.0 - erfc(x));
-            z = x * x;
-            y = x * polevl(z, T, 4) / p1evl(z, U, 5);
-            return y;
-        }
-        private static double p1evl(double x, double[] coef, int N)
-        {
-            double ans;
-
-            ans = x + coef[0];
-
-            for (int i = 1; i < N; i++)
-            {
-                ans = ans * x + coef[i];
-            }
-
-            return ans;
-        }
-
-        private static double polevl(double x, double[] coef, int N)
-        {
-            double ans;
-
-            ans = coef[0];
-
-            for (int i = 1; i <= N; i++)
-            {
-                ans = ans * x + coef[i];
-            }
-
-            return ans;
-        }
-        private const double MAXLOG = 7.09782712893383996732E2;
-        public static double erfc(double a)
-        {
-            double x, y, z, p, q;
-
-            double[] P =
-                {
-                    2.46196981473530512524E-10,
-                    5.64189564831068821977E-1,
-                    7.46321056442269912687E0,
-                    4.86371970985681366614E1,
-                    1.96520832956077098242E2,
-                    5.26445194995477358631E2,
-                    9.34528527171957607540E2,
-                    1.02755188689515710272E3,
-                    5.57535335369399327526E2
-                };
-            double[] Q =
-                {
-                    //1.0
-                    1.32281951154744992508E1,
-                    8.67072140885989742329E1,
-                    3.54937778887819891062E2,
-                    9.75708501743205489753E2,
-                    1.82390916687909736289E3,
-                    2.24633760818710981792E3,
-                    1.65666309194161350182E3,
-                    5.57535340817727675546E2
-                };
-
-            double[] R =
-                {
-                    5.64189583547755073984E-1,
-                    1.27536670759978104416E0,
-                    5.01905042251180477414E0,
-                    6.16021097993053585195E0,
-                    7.40974269950448939160E0,
-                    2.97886665372100240670E0
-                };
-            double[] S =
-                {
-                    //1.00000000000000000000E0, 
-                    2.26052863220117276590E0,
-                    9.39603524938001434673E0,
-                    1.20489539808096656605E1,
-                    1.70814450747565897222E1,
-                    9.60896809063285878198E0,
-                    3.36907645100081516050E0
-                };
-
-            if (a < 0.0)
-                x = -a;
-            else
-                x = a;
-
-            if (x < 1.0)
-                return 1.0 - erf(a);
-
-            z = -a * a;
-
-            if (z < -MAXLOG)
-            {
-                if (a < 0)
-                    return (2.0);
-                else
-                    return (0.0);
-            }
-
-            z = Math.Exp(z);
-
-            if (x < 8.0)
-            {
-                p = polevl(x, P, 8);
-                q = p1evl(x, Q, 8);
-            }
-            else
-            {
-                p = polevl(x, R, 5);
-                q = p1evl(x, S, 6);
-            }
-
-            y = (z * p) / q;
-
-            if (a < 0)
-                y = 2.0 - y;
-
-            if (y == 0.0)
-            {
-                if (a < 0)
-                    return 2.0;
-                else
-                    return (0.0);
-            }
-
-
-            return y;
-        }
+        private geffe geffe;
+        long ammount;
         private const string Pattern = "[^0-9x\\+]";
         private const string Pattern1 = "[^0-1]";
         private const string V = @".\randomInit\";
         private Random r = new Random();
-
-        static double Erf(double x)
-        {
-            // constants
-            double a1 = 0.254829592;
-            double a2 = -0.284496736;
-            double a3 = 1.421413741;
-            double a4 = -1.453152027;
-            double a5 = 1.061405429;
-            double p = 0.3275911;
-
-            // Save the sign of x
-            int sign = 1;
-            if (x < 0)
-                sign = -1;
-            x = Math.Abs(x);
-
-            // A&S formula 7.1.26
-            double t = 1.0 / (1.0 + p * x);
-            double y = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * Math.Exp(-x * x);
-
-            return sign * y;
-        }
 
         public MainWindow()
         {
@@ -242,7 +71,16 @@ namespace Generator
             (sender as TextBox).Text = "";
         }
 
-        private void generate_Click(object sender, RoutedEventArgs e)
+        private string gen(geffe g, long amm)
+        {
+            string to_return = "";
+            for (long i = 0; i < amm; ++i)
+                to_return += geffe.next().ToString();
+
+            return to_return;
+        }
+
+        private async void generate_Click(object sender, RoutedEventArgs e)
         {
             if (checkPoly(poly1.Text) == true &&
                 checkPoly(poly2.Text) == true &&
@@ -255,22 +93,21 @@ namespace Generator
                 lfsr _1 = new lfsr(poly1.Text, init1.Text);
                 lfsr _2 = new lfsr(poly2.Text, init2.Text);
                 lfsr _3 = new lfsr(poly3.Text, init3.Text);
-                geffe geffe = new geffe(_1, _2, _3);
-                long ammount = long.Parse(len.Text);
-                string to_return = "";
-                
-                for(long i = 0; i<ammount; ++i)
-                    to_return += geffe.next().ToString();
+                geffe = new geffe(_1, _2, _3);
+                ammount = long.Parse(len.Text);
 
-                result.Text = to_return;
+
+                string test = await Task.Run(() => gen(geffe, ammount));
+                result.Text = test;
+
 
                 using (System.IO.StreamWriter param3 = new System.IO.StreamWriter(@".\len.txt"))
                 {
                     param3.WriteLine(len.Text);
                 }
-                using (System.IO.StreamWriter fileResult = new System.IO.StreamWriter(@".\result.txt"))
+                using (System.IO.StreamWriter fileResult = new System.IO.StreamWriter(@".\" + number.Text + @".txt"))
                 {
-                    fileResult.Write(to_return);
+                    fileResult.Write(result.Text);
                 }
             }
             else
@@ -278,6 +115,7 @@ namespace Generator
                 var resut = MessageBox.Show("Coś poszło nie tak, sprawdź wszystkie pola jeszcze raz.");
             }
         }
+
 
         private void reset(object sender, RoutedEventArgs e)
         {
@@ -329,6 +167,7 @@ namespace Generator
         {
             int n = r.Next(1, 2900);
             string path = V + n.ToString();
+            number.Text = n.ToString();
             MessageBox.Show("Dane początkowe zostały pobrane z folderu: " + path);
             try
             {
@@ -413,26 +252,6 @@ namespace Generator
         private void monobit(object sender, RoutedEventArgs e)
         {
 
-            if(checkInit(result.Text) == true)
-            {
-                double sum = 0;
-                double size = result.Text.Length;
-                for (int i = 0; i<result.Text.Length; i++)
-                {
-                    if(result.Text[i] == '1')
-                        sum += 1;
-                    else
-                        sum -= 1;
-                }
-
-                double stat = (Math.Abs(sum) / Math.Sqrt(size));
-                double pVal = (stat / (Math.Sqrt(2)));
-                //System.Console.WriteLine(Er)
-                
-            }else
-            {
-                MessageBox.Show("Sprawdź poprawność wprowadzonych danych.");
-            }
         }
     }
 }
